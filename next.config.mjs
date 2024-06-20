494c75e49b382bf4cba34a995c3f484c9d069230
Ron Lancaster <2886602+nyvyn@users.noreply.github.com>
2024-06-20 06:57:50 -0500

    Resolves EmptyChannelError in LangGraph at build time.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+/** serverComponentsExternalPackage introduced on June 20, 2024 to resolve EmptyChannelError in LangGraph in NextJs Prod */
+/** Bug was not reproducable in Dev */
+const nextConfig = {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    experimental: {
+        serverComponentsExternalPackages: ["@langchain/langgraph"],
+    },
+}
 
 export default nextConfig;
